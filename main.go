4611475de649c1965e6e18c44c0a689ad0339a7d
Nikita Voloboev <nikita.voloboev@icloud.com>
2018-06-16 12:16:54 +0200

    refactor

--- main.go
+++ main.go
@@ -2,34 +2,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-// Defaults for Kingpin flags
-const (
-	defaultMaxResults = "100"
-)
-
-// Icons
 var (
+	// Icons
 	iconDefault = &aw.Icon{Value: "icon.png"}
 	iconUpdate  = &aw.Icon{Value: "icons/update-available.png"}
-)
 
-var (
-	// Kingpin and script options
-	app *kingpin.Application
-
-	// Application commands
-	searchCmd *kingpin.CmdClause
-	updateCmd *kingpin.CmdClause
-	testCmd   *kingpin.CmdClause
-
-	// Script options (populated by Kingpin application)
 	query string
 
 	repo = "nikitavoloboev/alfred-awesome-lists"
@@ -38,54 +19,14 @@ var (
 	wf *aw.Workflow
 )
 
-// Mostly sets up kingpin commands
 func init() {
 	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
-
-	app = kingpin.New("awesome", "Navigate awesome lists.")
-
-	// Update command
-	updateCmd = app.Command("update", "Check for new workflow version.").Alias("u")
-
-	// Commands using query
-	searchCmd = app.Command("search", "Search main awesome list.").Alias("s")
-
-	// Common options
-	for _, cmd := range []*kingpin.CmdClause{
-		searchCmd,
-	} {
-		cmd.Flag("query", "Search query.").Short('q').StringVar(&query)
-	}
 }
 
 func run() {
-	var err error
-
-	cmd, err := app.Parse(wf.Args())
-	if err != nil {
-		wf.FatalError(err)
-	}
-
-	switch cmd {
-	case searchCmd.FullCommand():
-		err = doSearch()
-	case updateCmd.FullCommand():
-		err = doUpdate()
-	default:
-		err = fmt.Errorf("Uknown command: %s", cmd)
-	}
-
-	// Check for update
-	if err == nil && cmd != updateCmd.FullCommand() {
-		err = checkForUpdate()
-	}
-
-	if err != nil {
-		wf.FatalError(err)
-	}
+	searchAwesomeLists()
 }
 
-// main wraps run() (actual entry point) to catch errors
 func main() {
 	wf.Run(run)
 }
--- search.go
+++ search.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
-	"github.com/nikitavoloboev/markdown-parser/parser"
+	"github.com/nikitavoloboev/markdown-parser"
 )
 
-func doSearch() error {
+func searchAwesomeLists() error {
 	showUpdateStatus()
 
 	log.Printf("query=%s", query)
 
-	// Parse URL for links
+	// Get the list from GitHub
 	links, err := parser.ParseMarkdownURL("https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md")
 	if err != nil {
 		log.Println("Error parsing links")
@@ -22,15 +23,16 @@ func doSearch() error {
 		wf.NewItem(k).Arg(v + "#readme").Valid(true).UID(k)
 	}
 
-	// TODO: Add cache
+	// TODO: add cache
 
 	// if err := wf.Session.LoadOrStoreJSON("awesome", getWins, &wins); err != nil {
 	// 	return nil, err
 	// }
 
+	query = os.Args[1]
+
 	if query != "" {
-		res := wf.Filter(query)
-		log.Printf("%d results for `%s`", len(res), query)
+		wf.Filter(query)
 	}
 
 	wf.WarnEmpty("No matching items", "Try a different query?")
